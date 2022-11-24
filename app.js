3437461e19ff4dcfd5045d0fd4c36e4ebe13f9d1
omarwebdeve <omarf977865@gmail.com>
2022-11-24 19:34:44 +0600

    user get

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
+// require('crypto').randomBytes(64).toString('hex')
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uadalh8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try {
+        const usersCollection = client.db('aspen-home').collection('users')
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+run()
 
 app.get('/',async(req,res)=> {
     res.send('Aspen Home server is running')
