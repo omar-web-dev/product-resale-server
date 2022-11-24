28830e720d77315a99b64c58be08e6e5932a8b9c
omarwebdeve <omarf977865@gmail.com>
2022-11-24 17:55:16 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/',async(req,res)=> {
+    res.send('Aspen Home server is running')
+})
+
+app.listen(port, ()=> console.log('server is running', port))
